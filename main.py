254d7904014ad0031314716ec4efbaa4c1e440cc
gmbrandt <gmbrandt@umail.ucsb.edu>
2019-04-03 14:42:34 -0700

    refactor so that all matrices and vectors are calculated once prior to fitting to a specific RA and DEC'

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ import matplotlib.pyplot as plt
 
 
 class IntermediateDataParser(object):
+    """
+    Base class for parsing Hip1 and Hip2 data. self.epoch, self.covariance_matrix and self.scan_angle are saved
+    as panda dataframes. use .values (e.g. self.epoch.values) to call the ndarray version.
+    """
     def __init__(self, scan_angle=None, covariance_matrix=None, epoch=None, residuals=None):
         self.scan_angle = scan_angle
         self.covariance_matrix = covariance_matrix
@@ -23,7 +27,7 @@ class IntermediateDataParser(object):
         filepath_list = glob.glob(filepath, recursive=True)
         if len(filepath_list) > 1:
             raise ValueError('More than one input file with hip id {0} found'.format(star_hip_id))
-        data = pd.read_csv(filepath_list[0], sep='[\s|]+', skiprows=skiprows, header=header)
+        data = pd.read_csv(filepath_list[0], sep='[\s|]+', skiprows=skiprows, header=header, engine='python')
         return data
 
     @abc.abstractmethod
@@ -31,7 +35,7 @@ class IntermediateDataParser(object):
         pass
 
     @staticmethod
-    def _convert_scan_angles_to_covariance_matrices(scan_angles):
+    def _calculate_covariance_matrices(scan_angles):
         pass
 
 
@@ -78,39 +82,123 @@ class HipparcosRereductionData(IntermediateDataParser):
 
 
 class AstrometricFitter(object):
-    def __init__(self, covariance_matrices, epoch_delta_times,
+    """
+    :param covariance_matrices: ndarray of length epoch times with 2x2 covariance matrices for each epoch
+    :param epoch_times: the times for each epoch.
+    """
+    def __init__(self, covariance_matrices, epoch_times,
                  astrometric_chi_squared_matrices=None, astrometric_solution_vector_components=None):
         self.covariance_matrices = covariance_matrices
-        self.epoch_delta_times = epoch_delta_times
+        self.epoch_times = epoch_times
         if astrometric_solution_vector_components is None:
-            self.astrometric_solution_vector_components = self._init_astrometric_solution_vectors
+            self.astrometric_solution_vector_components = self._init_astrometric_solution_vectors()
         if astrometric_chi_squared_matrices is None:
-            self.astrometric_chi_squared_matrices = self._init_astrometric_chi_squared_matrices
+            self.astrometric_chi_squared_matrices = self._init_astrometric_chi_squared_matrices()
 
     def fit_line(self, ra_vs_epoch, dec_vs_epoch):
-        pass
+        """
+        :param ra_vs_epoch: 1d array of right ascension, ordered the same as the covariance matrices and epochs.
+        :param dec_vs_epoch: 1d array of declination, ordered the same as the covariance matrices and epochs.
+        :return:
+        """
+        return np.linalg.solve(self._chi2_matrix(), self._chi2_vector(ra_vs_epoch=ra_vs_epoch,
+                                                                      dec_vs_epoch=dec_vs_epoch))
+
+    def _chi2_matrix(self):
+        return np.sum(self.astrometric_chi_squared_matrices, axis=0)
+
+    def _chi2_vector(self, ra_vs_epoch, dec_vs_epoch):
+        ra_solution_vecs = self.astrometric_solution_vector_components['ra']
+        dec_solution_vecs = self.astrometric_solution_vector_components['dec']
+        # sum together the individual solution vectors for each epoch
+        return np.dot(ra_vs_epoch, ra_solution_vecs) + np.dot(dec_vs_epoch, dec_solution_vecs)
 
     def _init_astrometric_solution_vectors(self):
-        pass
+        num_epochs = len(self.epoch_times)
+        astrometric_solution_vector_components = {'ra': np.zeros((num_epochs, 4)),
+                                                  'dec': np.zeros((num_epochs, 4))}
+        for epoch in range(num_epochs):
+            inverse_det_c = 1 / np.linalg.det(self.covariance_matrices[epoch])
+            a, b, c, d = unpack_elements_of_matrix(self.covariance_matrices[epoch])
+            epoch_time = self.epoch_times[epoch]
+            ra_vec, dec_vec = np.zeros(4).astype(np.float64), np.zeros(4).astype(np.float64)
+            ra_vec[0] = -inverse_det_c * (-2 * d * epoch_time)
+            ra_vec[1] = -inverse_det_c * ((b + c) * epoch_time)
+            ra_vec[2] = -inverse_det_c * (-2 * d)
+            ra_vec[3] = -inverse_det_c * (b + c)
+
+            dec_vec[0] = -inverse_det_c * ((b + c) * epoch_time)
+            dec_vec[1] = -inverse_det_c * (- 2 * a * epoch_time)
+            dec_vec[2] = -inverse_det_c * (b + c)
+            dec_vec[3] = -inverse_det_c * (- 2 * a)
+
+            astrometric_solution_vector_components['ra'][epoch] = ra_vec
+            astrometric_solution_vector_components['dec'][epoch] = dec_vec
+        return astrometric_solution_vector_components
 
     def _init_astrometric_chi_squared_matrices(self):
-        num_epochs = len(self.epoch_delta_times)
+        num_epochs = len(self.epoch_times)
         astrometric_chi_squared_matrices = np.zeros((num_epochs, 4, 4))
         for epoch in range(num_epochs):
-            astrometric_chi_squared_matrices[epoch] = None
+            inverse_det_c = 1 / np.linalg.det(self.covariance_matrices[epoch])
+            a, b, c, d = unpack_elements_of_matrix(self.covariance_matrices[epoch])
+            epoch_time = self.epoch_times[epoch]
+
+            A = np.zeros((4, 4))
+
+            A[:, 0] = inverse_det_c * np.array([2 * d * epoch_time,
+                                                (-b - c) * epoch_time,
+                                                2 * d,
+                                                (-b - c)])
+            A[:, 1] = inverse_det_c * np.array([(-b - c) * epoch_time,
+                                                2 * a * epoch_time,
+                                                (-b - c),
+                                                2 * a])
+            A[:, 2] = inverse_det_c * np.array([2 * d * epoch_time ** 2,
+                                                (-b - c) * epoch_time ** 2,
+                                                2 * d * epoch_time,
+                                                (-b - c) * epoch_time])
+            A[:, 3] = inverse_det_c * np.array([(-b - c) * epoch_time ** 2,
+                                                2 * a * epoch_time ** 2,
+                                                (-b - c) * epoch_time,
+                                                2 * a * epoch_time])
+
+            astrometric_chi_squared_matrices[epoch] = A
+        return astrometric_chi_squared_matrices
 
 
 def unpack_elements_of_matrix(matrix):
     return matrix.flatten()
 
 
-def chi2_matrix_single_epoch(covariance_matrix, epoch_delta_t, ra, dec):
+def chi2_vector_components_single_epoch(covariance_matrix, epoch_delta_t, ra, dec):
     """
     :param covariance_matrix:
     :param epoch_delta_t:
     :param ra:
     :param dec:
-    :return: the matrix A and vector c such that the solution v to A.v = c
+    :return: the vector c. Where the vector c and matrix A are such that the solution v to A.v = c
+    gives v = (mu_alpha, mu_delta, alpha_0, delta_0)
+    where alpha_0 and delta_0 are the mean RA and DEC of the star and
+    mu_alpha and mu_delta are the best fit proper motions in the RA and DEC
+    """
+    det_c = np.linalg.det(covariance_matrix)
+    a, b, c, d = unpack_elements_of_matrix(covariance_matrix)
+    vec = np.zeros(4).astype(np.float64)
+    vec[0] = -1/det_c*(-2*d*epoch_delta_t*ra + (b + c)*dec*epoch_delta_t)
+    vec[1] = -1/det_c*((b+c)*ra*epoch_delta_t - 2*a*dec * epoch_delta_t)
+    vec[2] = -1/det_c*(-2*d*ra+(b+c)*dec)
+    vec[3] = -1/det_c*((b+c)*ra-2*a*dec)
+    return vec
+
+
+def chi2_matrix_and_vec_old(covariance_matrix, epoch_delta_t, ra, dec):
+    """
+    :param covariance_matrix:
+    :param epoch_delta_t:
+    :param ra:
+    :param dec:
+    :return: the matrix A. Where the vector c and matrix A are such that the solution v to A.v = c
     gives v = (mu_alpha, mu_delta, alpha_0, delta_0)
     where alpha_0 and delta_0 are the mean RA and DEC of the star and
     mu_alpha and mu_delta are the best fit proper motions in the RA and DEC
@@ -143,32 +231,11 @@ def chi2_matrix_single_epoch(covariance_matrix, epoch_delta_t, ra, dec):
     return A, vec
 
 
-def chi2_vector_components_single_epoch(covariance_matrix, epoch_delta_t, ra, dec):
-    """
-    :param covariance_matrix:
-    :param epoch_delta_t:
-    :param ra:
-    :param dec:
-    :return: the matrix A and vector c such that the solution v to A.v = c
-    gives v = (mu_alpha, mu_delta, alpha_0, delta_0)
-    where alpha_0 and delta_0 are the mean RA and DEC of the star and
-    mu_alpha and mu_delta are the best fit proper motions in the RA and DEC
-    """
-    det_c = np.linalg.det(covariance_matrix)
-    a, b, c, d = unpack_elements_of_matrix(covariance_matrix)
-    vec = np.zeros(4).astype(np.float64)
-    vec[0] = -1/det_c*(-2*d*epoch_delta_t*ra + (b + c)*dec*epoch_delta_t)
-    vec[1] = -1/det_c*((b+c)*ra*epoch_delta_t - 2*a*dec * epoch_delta_t)
-    vec[2] = -1/det_c*(-2*d*ra+(b+c)*dec)
-    vec[3] = -1/det_c*((b+c)*ra-2*a*dec)
-    return vec
-
-
 def chi2_matrix_many_epochs(covariance_matrices, epoch_delta_ts, ras, decs):
     chi2_matrix = np.zeros((4, 4)).astype(np.float64)
     vec = np.zeros(4).astype(np.float64)
     for covariance_matrix, epoch_delta_t, ra, dec in zip(covariance_matrices, epoch_delta_ts, ras, decs):
-        single_epoch_chi2, single_epoch_vec = chi2_matrix_single_epoch(covariance_matrix, epoch_delta_t, ra, dec)
+        single_epoch_chi2, single_epoch_vec = chi2_matrix_and_vec_old(covariance_matrix, epoch_delta_t, ra, dec)
         chi2_matrix += single_epoch_chi2
         vec += single_epoch_vec
     return chi2_matrix, vec
@@ -188,57 +255,8 @@ def line_of_best_fit(astrometric_data):
 
 
 """
-unit tests
-"""
-
-
-def test_chi2_matrix_many_epochs():
-    # TODO
-    assert True
-
-
-def test_unpack_elements_of_matrix():
-    A = np.arange(4).reshape((2, 2))
-    assert np.allclose(np.arange(4), unpack_elements_of_matrix(A))
-    print('pass')
-
-
-def test_chi2_matrix_single_epoch():
-    covariance_matrix = np.array([[5, 1], [12, 2]])
-    ra, dec = 91, 82
-    epoch_delta_t = 30
-    A, c = chi2_matrix_single_epoch(covariance_matrix, epoch_delta_t, ra, dec)
-    expected_A = np.array([[-60, 195, -1800, 5850],
-                           [195, -150, 5850, -4500],
-                           [-2, 13/2, -60, 195],
-                           [13/2, -5, 195, -150]])
-    expected_c = (-1)*np.array([-10530, -5445, -351, -363/2])
-    assert np.allclose(expected_A, A)
-    assert np.allclose(expected_c, c)
-    print('pass')
-
-"""
 integration tests
 """
-
-
-def generate_linear_astrometric_data(correlation_coefficient=0.0, sigma_ra=0.1, sigma_dec=0.1):
-    astrometric_data = {}
-    num_measurements = 20
-    mu_ra, mu_dec = 1, 2
-    ra0, dec0 = 30, 40
-    epoch_start = 0
-    epoch_end = 200
-    astrometric_data['epoch_delta_t'] = np.linspace(epoch_start, epoch_end, num=num_measurements)
-    astrometric_data['dec'] = dec0 + astrometric_data['epoch_delta_t']*mu_dec
-    astrometric_data['ra'] = ra0 + astrometric_data['epoch_delta_t']*mu_ra
-    cc = correlation_coefficient
-    astrometric_data['covariance_matrix'] = np.zeros((num_measurements, 2, 2))
-    astrometric_data['covariance_matrix'][:] = np.array([[sigma_ra**2, sigma_ra*sigma_dec*cc],
-                                                       [sigma_ra*sigma_dec*cc, sigma_dec**2]])
-    astrometric_data['linear_solution_vector'] = np.array([ra0, dec0, mu_ra, mu_dec])
-
-    return astrometric_data
 
 
 def generate_parabolic_astrometric_data(correlation_coefficient=0.0, sigma_ra=0.1, sigma_dec=0.1, crescendo=False):
@@ -261,21 +279,17 @@ def generate_parabolic_astrometric_data(correlation_coefficient=0.0, sigma_ra=0.
     if crescendo:
         astrometric_data['covariance_matrix'][:, 0, 0] *= np.linspace(1/10, 4, num=num_measurements)
         astrometric_data['covariance_matrix'][:, 1, 1] *= np.linspace(4, 1/10, num=num_measurements)
-    astrometric_data['linear_solution_vector'] = np.array([ra0, dec0, mu_ra, mu_dec])
+    astrometric_data['solution_vector'] = np.array([ra0, dec0, mu_ra, mu_dec])
     return astrometric_data
-
-
-def test_fitting_to_linear_astrometric_data():
-    astrometric_data = generate_linear_astrometric_data(correlation_coefficient=0, sigma_ra=0.1, sigma_dec=0.1)
-    solution_vector = line_of_best_fit(astrometric_data)
-    assert np.allclose(solution_vector, astrometric_data['linear_solution_vector'])
-    print('pass')
 
 
 def plot_fitting_to_curved_astrometric_data(crescendo=False):
     astrometric_data = generate_parabolic_astrometric_data(correlation_coefficient=0, sigma_ra=5E2, sigma_dec=5E2, crescendo=crescendo)
     # solving
-    solution_vector = line_of_best_fit(astrometric_data)
+    fitter = AstrometricFitter(covariance_matrices=astrometric_data['covariance_matrix'],
+                               epoch_times=astrometric_data['epoch_delta_t'])
+    solution_vector = fitter.fit_line(ra_vs_epoch=astrometric_data['ra'],
+                                      dec_vs_epoch=astrometric_data['dec'])
     # plotting
     plt.figure()
     plt.errorbar(astrometric_data['epoch_delta_t'], astrometric_data['ra'],
@@ -296,17 +310,6 @@ def plot_fitting_to_curved_astrometric_data(crescendo=False):
 
 
 if __name__ == "__main__":
-    og = HipparcosOriginalData()
-    og.parse(star_hip_id='27321',
-             intermediate_data_directory='/home/mbrandt21/Downloads/Hip1/IntermediateData/',
-             data_choice='FAST')
-    rereduced = HipparcosRereductionData()
-    rereduced.parse(star_hip_id='27321',
-                    intermediate_data_directory='/home/mbrandt21/Downloads/Hip2/IntermediateData/resrec/')
-    print(og.epoch)
-    print(rereduced.epoch)
-    print(og.scan_angle)
-    print(rereduced.scan_angle)
-    #plot_fitting_to_curved_astrometric_data(crescendo=False)
-    #plot_fitting_to_curved_astrometric_data(crescendo=True)
-    #plt.show()
+    plot_fitting_to_curved_astrometric_data(crescendo=False)
+    plot_fitting_to_curved_astrometric_data(crescendo=True)
+    plt.show()
--- test_line_fitting.py
+++ test_line_fitting.py
@@ -9,19 +9,39 @@ def test_unpack_elements_of_matrix():
 
 def test_chi2_matrix_single_epoch():
     covariance_matrix = np.array([[5, 1], [12, 2]])
-    ra, dec = 91, 82
-    epoch_delta_t = 30
-    A, c = hipfit.chi2_matrix_single_epoch(covariance_matrix, epoch_delta_t, ra, dec)
+    epoch_time = 30
     expected_A = np.array([[-60, 195, -1800, 5850],
                            [195, -150, 5850, -4500],
                            [-2, 13/2, -60, 195],
                            [13/2, -5, 195, -150]])
+    fitter = hipfit.AstrometricFitter(covariance_matrices=[covariance_matrix], epoch_times=[epoch_time],
+                                      astrometric_solution_vector_components=[])
+    assert np.allclose(expected_A, fitter._chi2_matrix())
+
+
+def test_chi2_solution_vector_single_epoch():
+    covariance_matrix = np.array([[5, 1], [12, 2]])
+    ra, dec = 91, 82
+    epoch_time = 30
     expected_c = (-1)*np.array([-10530, -5445, -351, -363/2])
-    assert np.allclose(expected_A, A)
-    assert np.allclose(expected_c, c)
+    fitter = hipfit.AstrometricFitter(covariance_matrices=np.array([covariance_matrix]),
+                                      epoch_times=np.array([epoch_time]),
+                                      astrometric_chi_squared_matrices=[])
+    assert np.allclose(expected_c, fitter._chi2_vector(ra_vs_epoch=np.array([ra]),
+                                                       dec_vs_epoch=np.array([dec])))
+
+
+def test_fitting_to_linear_astrometric_data():
+    astrometric_data = generate_linear_astrometric_data(correlation_coefficient=0, sigma_ra=0.1, sigma_dec=0.1)
+    fitter = hipfit.AstrometricFitter(covariance_matrices=astrometric_data['covariance_matrix'],
+                                      epoch_times=astrometric_data['epoch_delta_t'])
+
+    assert np.allclose(fitter.fit_line(astrometric_data['ra'], astrometric_data['dec']),
+                       astrometric_data['linear_solution'])
+
 
 """
-integration tests
+Util functions
 """
 
 
@@ -39,7 +59,7 @@ def generate_linear_astrometric_data(correlation_coefficient=0.0, sigma_ra=0.1, 
     astrometric_data['covariance_matrix'] = np.zeros((num_measurements, 2, 2))
     astrometric_data['covariance_matrix'][:] = np.array([[sigma_ra**2, sigma_ra*sigma_dec*cc],
                                                        [sigma_ra*sigma_dec*cc, sigma_dec**2]])
-    astrometric_data['linear_solution_vector'] = np.array([ra0, dec0, mu_ra, mu_dec])
+    astrometric_data['linear_solution'] = np.array([ra0, dec0, mu_ra, mu_dec])
 
     return astrometric_data
 
@@ -64,11 +84,5 @@ def generate_parabolic_astrometric_data(correlation_coefficient=0.0, sigma_ra=0.
     if crescendo:
         astrometric_data['covariance_matrix'][:, 0, 0] *= np.linspace(1/10, 4, num=num_measurements)
         astrometric_data['covariance_matrix'][:, 1, 1] *= np.linspace(4, 1/10, num=num_measurements)
-    astrometric_data['linear_solution_vector'] = np.array([ra0, dec0, mu_ra, mu_dec])
+    astrometric_data['linear_solution'] = np.array([ra0, dec0, mu_ra, mu_dec])
     return astrometric_data
-
-
-def test_fitting_to_linear_astrometric_data():
-    astrometric_data = generate_linear_astrometric_data(correlation_coefficient=0, sigma_ra=0.1, sigma_dec=0.1)
-    solution_vector = hipfit.line_of_best_fit(astrometric_data)
-    assert np.allclose(solution_vector, astrometric_data['linear_solution_vector'])
