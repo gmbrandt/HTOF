09ef7eb3a5d7778c6c0da352eac0ab0bbacb8b0c
gmbrandt <gmbrandt@umail.ucsb.edu>
2019-04-05 19:38:22 -0700

    try reorganizing directories

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='HTOF',
+setup(name='htof',
       author=['G. Mirek Brandt, Daniel Michalik'],
       version='0.1.0',
       python_requires='>=3.5',
       packages=find_packages(),
+      package_dir={'HTOF': 'HTOF'},
       setup_requires=['pytest-runner'],
       install_requires=['numpy>=1.13', 'astropy', 'pandas', 'matplotlib'],
       tests_require=['pytest>=3.5'])
