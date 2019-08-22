fixed rotation matrix

htof/parse.py
@@ -86,9 +86,8 @@ def calculate_covariance_matrices(scan_angles, cross_scan_along_scan_var_ratio=1
     # we define the along scan to be 'y' in the scan basis.
     for theta in scan_angles.values.flatten():
         # see Eq 3 of F. Spoto et al.:GaiaData Release 2. Solar system
-        theta = np.pi/2 - theta  # to shift from angle between Dec and across-scan to angle between Dec and along-scan
         c, s = np.cos(theta), np.sin(theta)
-        Rccw = np.array([[c, -s], [s, c]])
+        Rccw = np.array([[s, c], [-c, s]])
         cov_matrix_in_ra_dec_basis = np.matmul(np.matmul(Rccw, cov_matrix_in_scan_basis), Rccw.T)
         covariance_matrices.append(cov_matrix_in_ra_dec_basis)
     return np.array(covariance_matrices)