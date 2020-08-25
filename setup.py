59576e54be5c0cb6342f0152782a889b79246404
gmbrandt <gmbrandt@ucsb.edu>
2020-08-25 12:56:44 -0700

    bumped version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(name='htof',
       author='G. Mirek Brandt, Daniel Michalik',
-      version='0.2.10',
+      version='0.2.11',
       python_requires='>=3.5',
       packages=find_packages(),
       package_dir={'htof': 'htof'},
