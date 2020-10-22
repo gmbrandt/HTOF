338ccbc4d24d1e0bd3e207e26e0177fb21a52753
gmbrandt <gmbrandt@ucsb.edu>
2020-10-22 10:41:03 -0700

    fixed the path to the dead time data

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='htof',
       python_requires='>=3.6',
       packages=find_packages(),
       package_dir={'htof': 'htof'},
-      package_data={'banzai_nres': ['data/hip1_flagged.txt', 'data/*.csv', 'data/hip2_dvd_flagged.fits']},
+      package_data={'htof': ['data/hip1_flagged.txt', 'data/*.csv', 'data/hip2_dvd_flagged.fits']},
       setup_requires=['pytest-runner'],
       install_requires=['astropy>=2.0', 'pandas>=0.24.0', 'scipy>=1.0.0', 'numpy>=1.16'],
       tests_require=['pytest>=3.5'])
